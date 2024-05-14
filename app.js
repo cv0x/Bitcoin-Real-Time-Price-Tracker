604d2a263d7652353fde8c3ad35f713a66f2bcd2
cv0x <71874388+cv0x@users.noreply.github.com>
2024-05-14 18:48:40 +0200

    Create Settings menu, font select
    creating a drop-down menu and setting the font change and saving to memory

--- app.js
+++ app.js
@@ -19,3 +19,42 @@ fetchBtcPrice();
 
 // Update BTC price every 10 seconds
 setInterval(fetchBtcPrice, 10000);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const fontSelect = document.getElementById("font-family");
+  const fontShadow = document.getElementById("font-shadow");
+  const saveButton = document.getElementById("save");
+
+  // Function to save the selected font
+  saveButton.addEventListener("click", function () {
+    const selectedFont = fontSelect.value;
+    document.body.style.fontFamily = selectedFont;
+
+    //Store the selected font in memory
+    localStorage.setItem("selectedFont", selectedFont);
+  });
+
+  //Load saved font on page load
+  const savedFont = localStorage.getItem("selectedFont");
+  if (savedFont) {
+    document.body.style.fontFamily = savedFont;
+    fontSelect.value = savedFont;
+  }
+
+  fontSelect.addEventListener("change", function () {
+    const selectedFont = fontSelect.value;
+    //Set the selected font
+    document.body.style.fontFamily = selectedFont;
+  });
+
+  //Toggle icon menu
+  const menuIcon = document.getElementById("menu-icon");
+  const menu = document.querySelector(".menu");
+
+  menuIcon.addEventListener("click", function () {
+    menu.style.display =
+      menu.style.display === "none" || menu.style.display === ""
+        ? "block"
+        : "none";
+  });
+});
